f7ea379eb17b71c2ecd6a434624d96a47b8fc4b6
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:05:53 +0300

    Add files via upload

---
+++ 5.55/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5._55
+{
+    class Program
+    {
+        static double Metod(string Z, double Min = -1.7E308, double Max = 1.7E308, double Exception1 = -1.7E308, double Exception2 = -1.7E308)
+        {
+            Console.WriteLine($"Введите {Z}:");
+            double A;
+            while (!(double.TryParse(Console.ReadLine(), out A) && A >= Min && A <= Max && A != Exception1 && A != Exception2));
+            return A;
+        }
+        static void Main()
+        {
+            int B = (int)Metod("количество предметов", 1);
+            double C = 0;
+            for (int D = 1; D <= B; D++) C += Metod($"массу {D} предмета");
+            Console.WriteLine(C / B);
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+    }
